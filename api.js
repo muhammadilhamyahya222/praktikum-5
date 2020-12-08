ee8b463ac30e021101ceba6e6486f6991664c04d
muhammadilhamyahya222 <62550125+muhammadilhamyahya222@users.noreply.github.com>
2020-12-08 21:19:14 +0700

    second commit

--- api.js
+++ api.js
@@ -1,33 +1,209 @@
-// inisiasi library
 const express = require("express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
+const app = express()
+const multer = require("multer") // untuk upload file
+const path = require("path") // untuk memanggil path direktori
+const fs = require("fs") // untuk manajemen file
 const mysql = require("mysql")
-const moment = require("moment")
+const cors = require("cors")
 const md5 = require("md5")
 const Cryptr = require("cryptr")
-const crypt = new Cryptr("121314151617") 
-// implementation
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+const crypt = new Cryptr("140533601726")
 
-// create MySQL Connection
+app.use(express.static(__dirname));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        // set file storage
+        cb(null, './image');
+    },
+    filename: (req, file, cb) => {
+        // generate file name 
+        cb(null, "image-"+ Date.now() + path.extname(file.originalname))
+    }
+})
+
+let upload = multer({storage: storage})
+
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "pelanggaran_siswa"
+    database: "olshop"
 })
 
-db.connect(error => {
-    if (error) {
-        console.log(error.message)
+// endpoint login user (authentication)
+app.post("/admin/auth", (req, res) => {
+    // tampung username dan password
+    let param = [
+        req.body.username, //username
+        md5(req.body.password) // password
+    ]
+    
+
+    // create sql query
+    let sql = "select * from admin where username = ? and password = ?"
+
+    // run query
+    db.query(sql, param, (error, result) => {
+        if (error) throw error
+
+        // cek jumlah data hasil query
+        if (result.length > 0) {
+            // user tersedia
+            res.json({
+                message: "Logged",
+                token: crypt.encrypt(result[0].id_admin), // generate token
+                data: result
+            })
+        } else {
+            // user tidak tersedia
+            res.json({
+                message: "Invalid username/password"
+            })
+        }
+    })
+})
+
+
+// endpoint untuk menambah data barang baru
+app.post("/barang", upload.single("image"), (req, res) => {
+    // prepare data
+    let data = {
+        nama_barang: req.body.nama_barang,
+        harga: req.body.harga,
+        stok: req.body.stok,
+        deskripsi: req.body.deskripsi,
+        image: req.file.filename
+    }
+
+    if (!req.file) {
+        // jika tidak ada file yang diupload
+        res.json({
+            message: "Tidak ada file yang dikirim"
+        })
     } else {
-        console.log("MySQL Connected")
+        // create sql insert
+        let sql = "insert into barang set ?"
+
+        // run query
+        db.query(sql, data, (error, result) => {
+            if(error) throw error
+            res.json({
+                message: result.affectedRows + " data berhasil disimpan"
+            })
+        })
     }
 })
+
+// endpoint untuk mengubah data barang
+app.put("/barang", upload.single("image"), (req,res) => {
+    let data = null, sql = null
+    // paramter perubahan data
+    let param = { kode_barang: req.body.kode_barang }
+
+    if (!req.file) {
+        // jika tidak ada file yang dikirim = update data saja
+        data = {
+            nama_barang: req.body.nama_barang,
+            harga: req.body.harga,
+            stok: req.body.stok,
+            deskripsi: req.body.deskripsi
+        }
+    } else {
+        // jika mengirim file = update data + reupload
+        data = {
+            nama_barang: req.body.nama_barang,
+            harga: req.body.harga,
+            stok: req.body.stok,
+            deskripsi: req.body.deskripsi,
+            image: req.file.filename
+        }
+
+        // get data yg akan diupdate utk mendapatkan nama file yang lama
+        sql = "select * from barang where ?"
+        // run query
+        db.query(sql, param, (err, result) => {
+            if (err) throw err
+            // tampung nama file yang lama
+            let fileName = result[0].image
+
+            // hapus file yg lama
+            let dir = path.join(__dirname,"image",fileName)
+            fs.unlink(dir, (error) => {})
+        })
+
+    }
+
+    // create sql update
+    sql = "update barang set ? where ?"
+
+    // run sql update
+    db.query(sql, [data,param], (error, result) => {
+        if (error) {
+            res.json({
+                message: error.message
+            })
+        } else {
+            res.json({
+                message: result.affectedRows + " data berhasil diubah"
+            })
+        }
+    })
+})
+
+// endpoint untuk menghapus data barang
+app.delete("/barang/:kode_barang", (req,res) => {
+    let param = {kode_barang: req.params.kode_barang}
+
+    // ambil data yang akan dihapus
+    let sql = "select * from barang where ?"
+    // run query
+    db.query(sql, param, (error, result) => {
+        if (error) throw error
+        
+        // tampung nama file yang lama
+        let fileName = result[0].image
+
+        // hapus file yg lama
+        let dir = path.join(__dirname,"image",fileName)
+        fs.unlink(dir, (error) => {})
+    })
+
+    // create sql delete
+    sql = "delete from barang where ?"
+
+    // run query
+    db.query(sql, param, (error, result) => {
+        if (error) {
+            res.json({
+                message: error.message
+            })
+        } else {
+            res.json({
+                message: result.affectedRows + " data berhasil dihapus"
+            })
+        }      
+    })
+})
+
+// endpoint ambil data barang
+app.get("/barang",validateToken(), (req, res) => {
+    // create sql query
+    let sql = "select * from barang"
+
+    // run query
+    db.query(sql, (error, result) => {
+        if (error) throw error
+        res.json({
+            data: result,
+            count: result.length
+        })
+    })
+})
+
 validateToken = () => {
     return (req, res, next) => {
         // cek keberadaan "Token" pada request header
@@ -40,19 +216,19 @@ validateToken = () => {
             // tampung nilai Token
             let token  = req.get("Token")
             
-            // decrypt token menjadi id_user
+            // decrypt token menjadi id_admin
             let decryptToken = crypt.decrypt(token)
 
-            // sql cek id_user
-            let sql = "select * from user where ?"
+            // sql cek id_admin
+            let sql = "select * from admin where ?"
 
             // set parameter
-            let param = { id_user: decryptToken}
+            let param = { id_admin: decryptToken}
 
             // run query
             db.query(sql, param, (error, result) => {
                 if (error) throw error
-                 // cek keberadaan id_user
+                 // cek keberadaan id_admin
                 if (result.length > 0) {
                     // id_user tersedia
                     next()
@@ -68,299 +244,7 @@ validateToken = () => {
     }
 }
 
-// endpoint login user (authentication)
-app.post("/user/auth", (req, res) => {
-    // tampung username dan password
-    let param = [
-        req.body.username, //username
-        md5(req.body.password) // password
-    ]
-    
 
-    // create sql query
-    let sql = "select * from user where username = ? and password = ?"
-
-    // run query
-    db.query(sql, param, (error, result) => {
-        if (error) throw error
-
-        // cek jumlah data hasil query
-        if (result.length > 0) {
-            // user tersedia
-            res.json({
-                message: "Logged",
-                token: crypt.encrypt(result[0].id_user), // generate token
-                data: result
-            })
-        } else {
-            // user tidak tersedia
-            res.json({
-                message: "Invalid username/password"
-            })
-        }
-    })
-})
-
-// end-point akses data siswa
-app.get("/siswa", validateToken(), (req, res) => {
-    // create sql query
-    let sql = "select * from siswa"
-
-    // run query
-    db.query(sql, (error, result) => {
-        let response = null
-        if (error) {
-            response = {
-                message: error.message // pesan error
-            }            
-        } else {
-            response = {
-                count: result.length, // jumlah data
-                siswa: result // isi data
-            }            
-        }
-        res.json(response) // send response
-    })
-})
-
-// end-point akses data siswa berdasarkan id_siswa tertentu
-app.get("/siswa/:id", (req, res) => {
-    let data = {
-        id_siswa: req.params.id
-    }
-    // create sql query
-    let sql = "select * from siswa where ?"
-
-    // run query
-    db.query(sql, data, (error, result) => {
-        let response = null
-        if (error) {
-            response = {
-                message: error.message // pesan error
-            }            
-        } else {
-            response = {
-                count: result.length, // jumlah data
-                siswa: result // isi data
-            }            
-        }
-        res.json(response) // send response
-    })
-})
-
-// end-point menyimpan data siswa
-app.post("/siswa", (req,res) => {
-
-    // prepare data
-    let data = {
-        nis: req.body.nis,
-        nama_siswa: req.body.nama_siswa,
-        kelas: req.body.kelas,
-        poin: req.body.poin
-    }
-
-    // create sql query insert
-    let sql = "insert into siswa set ?"
-
-    // run query
-    db.query(sql, data, (error, result) => {
-        let response = null
-        if (error) {
-            response = {
-                message: error.message
-            }
-        } else {
-            response = {
-                message: result.affectedRows + " data inserted"
-            }
-        }
-        res.json(response) // send response
-    })
-})
-
-// end-point mengubah data siswa
-app.put("/siswa", (req,res) => {
-
-    // prepare data
-    let data = [
-        // data
-        {
-            nis: req.body.nis,
-            nama_siswa: req.body.nama_siswa,
-            kelas: req.body.kelas,
-            poin: req.body.poin
-        },
-
-        // parameter (primary key)
-        {
-            id_siswa: req.body.id_siswa
-        }
-    ]
-
-    // create sql query update
-    let sql = "update siswa set ? where ?"
-
-    // run query
-    db.query(sql, data, (error, result) => {
-        let response = null
-        if (error) {
-            response = {
-                message: error.message
-            }
-        } else {
-            response = {
-                message: result.affectedRows + " data updated"
-            }
-        }
-        res.json(response) // send response
-    })
-})
-
-// end-point menghapus data siswa berdasarkan id_siswa
-app.delete("/siswa/:id", (req,res) => {
-    // prepare data
-    let data = {
-        id_siswa: req.params.id
-    }
-
-    // create query sql delete
-    let sql = "delete from siswa where ?"
-
-    // run query
-    db.query(sql, data, (error, result) => {
-        let response = null
-        if (error) {
-            response = {
-                message: error.message
-            }
-        } else {
-            response = {
-                message: result.affectedRows + " data deleted"
-            }
-        }
-        res.json(response) // send response
-    })
-})
-
-// end-point menambahkan data pelanggaran siswa 
-
-app.post("/pelanggaran_siswa", (req,res) => {
-    // prepare data to pelanggaran_siswa
-    let data = {
-        id_siswa: req.body.id_siswa,
-        id_user: req.body.id_user,
-        waktu: moment().format('YYYY-MM-DD HH:mm:ss') // get current time
-    }
-
-    // parse to JSON
-    let pelanggaran = JSON.parse(req.body.pelanggaran)
-
-    // create query insert to pelanggaran_siswa
-    let sql = "insert into pelanggaran_siswa set ?"
-
-    // run query
-    db.query(sql, data, (error, result) => {
-        let response = null
-        
-        if (error) {
-            res.json({message: error.message})
-        } else {
-            
-            // get last inserted id_pelanggaran
-            let lastID = result.insertId
-
-            // prepare data to detail_pelanggaran
-            let data = []
-            for (let index = 0; index < pelanggaran.length; index++) {
-                data.push([
-                    lastID, pelanggaran[index].id_pelanggaran
-                ])                
-            }
-
-            // create query insert detail_pelanggaran
-            let sql = "insert into detail_pelanggaran_siswa values ?"
-
-            db.query(sql, [data], (error, result) => {
-                if (error) {
-                    res.json({message: error.message})
-                } else {
-                    res.json({message: "Data has been inserted"})
-                }
-            })
-        }
-    })
-})
-
-// end-point menampilkan data pelanggaran siswa
-app.get("/pelanggaran_siswa", (req,res) => {
-    // create sql query
-    let sql = "select p.id_pelanggaran_siswa, p.id_siswa,p.waktu, s.nis, s.nama_siswa, p.id_user, u.nama_user " +
-     "from pelanggaran_siswa p join siswa s on p.id_siswa = s.id_siswa " +
-     "join user u on p.id_user = u.id_user"
-
-    // run query
-    db.query(sql, (error, result) => {
-        if (error) {
-            res.json({ message: error.message})   
-        }else{
-            res.json({
-                count: result.length,
-                pelanggaran_siswa: result
-            })
-        }
-    })
-})
-
-// end-point untuk menampilkan detail pelanggaran
-app.get("/pelanggaran_siswa/:id_pelanggaran_siswa", (req,res) => {
-    let param = { id_pelanggaran_siswa: req.params.id_pelanggaran_siswa}
-
-    // create sql query
-    let sql = "select p.nama_pelanggaran, p.poin " + 
-    "from detail_pelanggaran_siswa dps join pelanggaran p "+
-    "on p.id_pelanggaran = dps.id_pelanggaran " +
-    "where ?"
-
-    db.query(sql, param, (error, result) => {
-        if (error) {
-            res.json({ message: error.message})   
-        }else{
-            res.json({
-                count: result.length,
-                detail_pelanggaran_siswa: result
-            })
-        }
-    })
-})
-
-// end-point untuk menghapus data pelanggaran_siswa
-app.delete("/pelanggaran_siswa/:id_pelanggaran_siswa", (req, res) => {
-    let param = { id_pelanggaran_siswa: req.params.id_pelanggaran_siswa}
-
-    // create sql query delete detail_pelanggaran
-    let sql = "delete from detail_pelanggaran_siswa where ?"
-
-    db.query(sql, param, (error, result) => {
-        if (error) {
-            res.json({ message: error.message})
-        } else {
-            let param = { id_pelanggaran_siswa: req.params.id_pelanggaran_siswa}
-            // create sql query delete pelanggaran_siswa
-            let sql = "delete from pelanggaran_siswa where ?"
-
-            db.query(sql, param, (error, result) => {
-                if (error) {
-                    res.json({ message: error.message})
-                } else {
-                    res.json({message: "Data has been deleted"})
-                }
-            })
-        }
-    })
-
-})
-
-
-app.listen(8000, () => {
-    console.log("Run on port 8000")
+app.listen(8000, () =>{
+    console.log("Server run on port 8000");
 })
